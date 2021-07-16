c92c46956b3b63c3bdde781cfac1db579f1dd985
akhilkumarmengani@gmail.com <akhilkumarmengani@gmail.com>
2021-07-16 12:46:36 +0530

    home routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { WorkExpComponent } from './components/work-exp/work-exp.component';
 
 const routes: Routes = [
+  {path:'', component: PresentLifeComponent, data: { animationState: 'home' }},
   {path:'home', component: PresentLifeComponent , data: { animationState: 'home' }},
   {path:'workexperience',component: WorkExpComponent,data: { animationState: 'work' }},
   {path:'projects',component: ProjectsComponent,data: { animationState: 'projects' }}
