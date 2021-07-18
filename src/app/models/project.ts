aa2425ef97038f7b3bf19c5255b736d3fe318be4
akhilkumarmengani@gmail.com <akhilkumarmengani@gmail.com>
2021-07-18 23:46:28 +0530

    project model

---
+++ src/app/models/project.ts
@@ -0,0 +1,9 @@
+export class Project{
+    title : String;
+    description : String[];
+
+    constructor(title:String , description : String[]){
+        this.title = title;
+        this.description = [];
+    }
+}
