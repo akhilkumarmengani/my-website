d064b6c0508c94ae7882c078ad3b01148c183b06
akhilkumarmengani@gmail.com <akhilkumarmengani@gmail.com>
2021-08-15 00:15:29 -0400

    added contact section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
 import { MainComponent } from './components/main/main.component';
 import { PresentLifeComponent } from './components/present-life/present-life.component';
 import { ProjectsComponent } from './components/projects/projects.component';
@@ -8,7 +9,8 @@ import { WorkExpComponent } from './components/work-exp/work-exp.component';
 const routes: Routes = [
   {path:'home', component: PresentLifeComponent , data: { animationState: 'home' }},
   {path:'workexperience',component: WorkExpComponent,data: { animationState: 'work' }},
-  {path:'projects',component: ProjectsComponent,data: { animationState: 'projects' }}
+  {path:'projects',component: ProjectsComponent,data: { animationState: 'projects' }},
+  {path:'contact',component: ContactComponent,data: { animationState: 'contact' }}
 ];
 
 @NgModule({
